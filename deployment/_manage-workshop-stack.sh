f421513c28e42c6dad01fd7f0e0daf9cb66e2b3b
EC2 Default User <ec2-user@ip-10-0-2-61.eu-central-1.compute.internal>
2024-06-03 15:31:23 +0000

    update

--- deployment/_manage-workshop-stack.sh
+++ deployment/_manage-workshop-stack.sh
@@ -24,18 +24,8 @@ create_workshop() {
 }
 
 delete_workshop() {
-    bootstrap_cdk
     get_c9_id
-    if [[ "$C9_ID" != "None" ]]; then
-        aws ec2 start-instances --instance-ids "$C9_ID"
-        wait_for_instance_ssm "$C9_ID"
-        # run_ssm_command "cd ~/environment/$REPO_NAME/deployment && ./delete-workshop.sh -s | tee .workshop.out"
-        ./delete-workshop.sh -s
-    else
-        cd ..
-        ./delete-workshop.sh -s
-        cd cloud9
-    fi
+    ./delete-workshop.sh
     
     aws ec2 create-tags --resources $C9_ID --tags "Key=Workshop,Value=${WORKSHOP_NAME}Old"
     cd cloud9
