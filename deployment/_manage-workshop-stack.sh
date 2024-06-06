39c1e09e571f6ab5e17303cefb04d678dbe5e1ab
EC2 Default User <ec2-user@ip-10-0-2-61.eu-central-1.compute.internal>
2024-06-06 19:58:25 +0000

    update

--- deployment/_manage-workshop-stack.sh
+++ deployment/_manage-workshop-stack.sh
@@ -23,34 +23,7 @@ create_workshop() {
 }
 
 delete_workshop() {
-    get_c9_id
-    aws ec2 create-tags --resources $C9_ID --tags "Key=Workshop,Value=${WORKSHOP_NAME}Old"
     echo "Deleting workshop"
-
-    TRAILS=( "saas-ops-ddb-access-trails" "saas-ops-management-trails" )
-    STACKS_1=()
-    STACKS_2=( "saas-operations-controlplane" "saas-operations-pipeline" "saasOpsWorkshop-saasOperationsDashboard" "${WORKSHOP_NAME}-C9")
-    CODECOMMIT_REPOS=( "saas-operations-workshop" )
-
-    for TRAIL in "${TRAILS[@]}"; do
-        stop_cloudtrail "${TRAIL}"
-    done
-    delete_tenant_stacks
-    delete_buckets
-    for STACK in "${STACKS_1[@]}"; do
-        delete_stack "${STACK}"
-    done
-    for STACK in "${STACKS_2[@]}"; do
-        delete_stack "${STACK}" &
-    done
-    wait_for_background_jobs
-    for REPO in "${CODECOMMIT_REPOS[@]}"; do
-        delete_codecommit_repo "${REPO}" &
-    done
-    delete_log_groups &
-    delete_user_pools &
-    delete_api_keys &
-    wait_for_background_jobs
-
+    ./delete-workshop.sh
     echo "Workshop deleted"
 }
---
+++ deployment/delete_workshop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+TRAILS=( "saas-ops-ddb-access-trails" "saas-ops-management-trails" )
+STACKS_1=()
+STACKS_2=( "saas-operations-controlplane" "saas-operations-pipeline" "saasOpsWorkshop-saasOperationsDashboard" "${WORKSHOP_NAME}-C9")
+CODECOMMIT_REPOS=( "saas-operations-workshop" )
+
+get_c9_id
+aws ec2 create-tags --resources $C9_ID --tags "Key=Workshop,Value=${WORKSHOP_NAME}Old"
+
+for TRAIL in "${TRAILS[@]}"; do
+    stop_cloudtrail "${TRAIL}"
+done
+delete_tenant_stacks
+delete_buckets
+for STACK in "${STACKS_1[@]}"; do
+    delete_stack "${STACK}"
+done
+for STACK in "${STACKS_2[@]}"; do
+    delete_stack "${STACK}" &
+done
+wait_for_background_jobs
+for REPO in "${CODECOMMIT_REPOS[@]}"; do
+    delete_codecommit_repo "${REPO}" &
+done
+delete_log_groups &
+delete_user_pools &
+delete_api_keys &
+wait_for_background_jobs
